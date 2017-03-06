6fa9e279217f8ce64999b8118e1e07180e743278
philpeace <phil.totheworld@gmail.com>
2017-03-06 12:43:37 +0000

    Upgrading to .NET Core 1.0.1 and removing IIS EXpress config

--- Dtl.FrontEndTest/Program.cs
+++ Dtl.FrontEndTest/Program.cs
@@ -14,7 +14,7 @@ namespace Dtl.FrontEndTest
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+                //.UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
--- Dtl.FrontEndTest/Startup.cs
+++ Dtl.FrontEndTest/Startup.cs
@@ -40,7 +40,6 @@ namespace Dtl.FrontEndTest
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
             }
             else
             {
